af96b80a426bef7a38735acd74272a3dd5b4f4a1
andresfccyFSL <acardenas@fullstacklabs.co>
2021-10-26 04:31:24 -0500

    Done

--- src/components/AutocorrectTextarea.js
+++ src/components/AutocorrectTextarea.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-class AutocorrectTextarea extends React.Component {
+const correctionsList = {
+  'wierd': 'weird',
+  'wolrd': 'world',
+  'reaily': 'really'
+};
 
-  render() {
-    return (
-      <div className="text-center">
-        <textarea data-testid="textarea" rows={10} cols={80} className="card" />
-      </div>
-    );
+const AutocorrectTextarea = () => {
+  const [text, setText] = useState('');
+
+  const handleChange = (e) => {
+    setText(e.target.value);
   }
-}
+
+  const handleSpace = (e) => {
+    if(e.keyCode === 32) {
+      const inputWordsList = text.split(' ');
+      setText(inputWordsList.map((o)=>{
+        return correctionsList[o] || o;
+      }).join(' '));
+    }
+  }
+
+  return (
+    <div className="text-center">
+      <textarea 
+        data-testid="textarea" 
+        rows={10} 
+        cols={80} 
+        className="card"
+        value={text}
+        onChange={handleChange}
+        onKeyDown={handleSpace} />
+    </div>
+  );
+};
 
 export default AutocorrectTextarea;
